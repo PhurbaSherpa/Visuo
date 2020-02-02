bbe277fbfb77aa16a8e9b4bab2e05a9a9044e1a5
Phurba Sherpa <srpphurba@gmail.com>
2020-02-02 14:40:46 -0500

    added bubble sort algo

---
+++ src/algorithims/bubbleSort.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr) {
+  for (let j = arr.length - 1; j > 0; j--) {
+    let swapped = false;
+    for (let i = 0; i < j; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+    if (!swapped) break;
+  }
+
+  return arr;
+}
