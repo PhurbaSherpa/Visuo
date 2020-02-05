41bfc6a2712f9bc2aa2d74e46f715c28872b1e56
Phurba Sherpa <srpphurba@gmail.com>
2020-02-04 22:05:24 -0500

    update sort timer

--- src/Graph.js
+++ src/Graph.js
@@ -41,7 +41,7 @@ export default function Graph(props) {
                 setPermanent([...permanent]);
                 setArr([...newArr]);
               }
-            }, 1000);
+            }, 500);
           }}
         >
           Sort
