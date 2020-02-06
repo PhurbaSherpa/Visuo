47929829960003cc5ddd9c7d25e9d4bd6e0ee106
Phurba Sherpa <srpphurba@gmail.com>
2020-02-06 11:09:30 -0500

    updated number of bars

--- src/algorithims/generateNewArr.js
+++ src/algorithims/generateNewArr.js
@@ -1,6 +1,6 @@
-function generateNewArr(num = 100) {
-  let arr = new Array(20).fill(0).map(function(n) {
-    return Math.floor(Math.random() * (num - 1) + 1);
+function generateNewArr() {
+  let arr = new Array(50).fill(0).map(function(n) {
+    return Math.floor(Math.random() * (100 - 1) + 1);
   });
   return arr;
 }
